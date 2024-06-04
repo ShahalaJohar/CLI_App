219c5c9f21ecd9365671f403627a91e4de94d7ef
ShahalaJohar <shahalajohar@gmail.com>
2024-06-04 19:13:17 +0200

    do main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def main ():
+
+
+
+      bookList = []
+      
+      choice = 0
+      while choice != 4:
+        print("-----Book Manager-----")
+        print("1) Add a book")
+        print("2) LookUp a book")
+        print("3) Display all books")
+        print("4) Quit")
+        choice = int(input())
+
+
+
+
+if __name__ == "__main__" :  
+    main()    
